30f245b27c85599dd2f67bfeb11faa0fc8cf40ad
Josh Waller <mdx.programs@gmail.com>
2022-07-21 15:42:22 -0400

    adds pug plugin and changes double to single quotes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,40 +8,40 @@ augroup end
 ]]
 
 local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap =
-    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-return require("packer").startup(
+return require('packer').startup(
     function()
-        use "LnL7/vim-nix"
-        use "SirVer/ultisnips"
-        use "airblade/vim-gitgutter"
-        use "alaviss/nim.nvim"
-        use "azadkuh/vim-cmus"
-        use "fatih/vim-go"
-        use "honza/vim-snippets"
-        use "jiangmiao/auto-pairs"
-        use "luochen1990/rainbow"
-        use "mattn/emmet-vim"
-        use "mbbill/undotree"
-        use "nathanaelkane/vim-indent-guides"
-        use "nvim-lua/plenary.nvim"
-        use "othree/html5.vim"
-        use "pangloss/vim-javascript"
-        use "preservim/nerdcommenter"
-        use "radenling/vim-dispatch-neovim"
-        use "rhysd/vim-grammarous"
-        use "ryanoasis/vim-devicons"
-        use "sheerun/vim-polyglot"
-        use "stephenway/postcss.vim"
-        use "tpope/vim-dispatch"
-        use "tpope/vim-surround"
-        use "vim-airline/vim-airline"
-        use "vim-utils/vim-man"
-        use "wbthomason/packer.nvim"
+        use 'LnL7/vim-nix'
+        use 'SirVer/ultisnips'
+        use 'airblade/vim-gitgutter'
+        use 'alaviss/nim.nvim'
+        use 'azadkuh/vim-cmus'
+        use 'fatih/vim-go'
+        use 'honza/vim-snippets'
+        use 'jiangmiao/auto-pairs'
+        use 'luochen1990/rainbow'
+        use 'mattn/emmet-vim'
+        use 'mbbill/undotree'
+        use 'nathanaelkane/vim-indent-guides'
+        use 'nvim-lua/plenary.nvim'
+        use 'othree/html5.vim'
+        use 'pangloss/vim-javascript'
+        use 'preservim/nerdcommenter'
+        use 'radenling/vim-dispatch-neovim'
+        use 'rhysd/vim-grammarous'
+        use 'ryanoasis/vim-devicons'
+        use 'sheerun/vim-polyglot'
+        use 'stephenway/postcss.vim'
+        use 'tpope/vim-dispatch'
+        use 'tpope/vim-surround'
+        use 'vim-airline/vim-airline'
+        use 'vim-utils/vim-man'
+        use 'wbthomason/packer.nvim'
         use 'MunifTanjim/nui.nvim'
         use 'editorconfig/editorconfig-vim'
         use 'hrsh7th/cmp-buffer'
@@ -52,12 +52,13 @@ return require("packer").startup(
         use 'hrsh7th/nvim-cmp'
         use 'lepture/vim-jinja'
         use 'neovim/nvim-lspconfig'
+        use 'digitaltoad/vim-pug'
         use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-        use {"dracula/vim", name = "vim-dracula", rev = "bfbc3cadbd142e74d3b92e63f1de8711261015a4", frozen = 1}
-        use {"evanleck/vim-svelte", branch = "main"}
-        use {"neoclide/coc.nvim", branch = "release"}
-        use {"nvim-telescope/telescope.nvim", requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}}
-        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+        use {'dracula/vim', name = 'vim-dracula', rev = 'bfbc3cadbd142e74d3b92e63f1de8711261015a4', frozen = 1}
+        use {'evanleck/vim-svelte', branch = 'main'}
+        use {'neoclide/coc.nvim', branch = 'release'}
+        use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
+        use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
         use {
             'goolord/alpha-nvim',
             requires = { 'kyazdani42/nvim-web-devicons' },
@@ -66,7 +67,7 @@ return require("packer").startup(
             end
         }
         if packer_bootstrap then
-            require("packer").sync()
+            require('packer').sync()
         end
     end
     )
